328b7660e3bb7ffb1f404d1314680fa99fcb5415
Jeff T <jeff.thao92@gmail.com>
2020-12-01 21:21:56 -0600

    Added a sidebar into navBar js file.

--- client/src/components/client-portal/NavBar/index.js
+++ client/src/components/client-portal/NavBar/index.js
@@ -4,6 +4,7 @@ import { Nav, Navbar } from 'react-bootstrap';
 // Navigation Bar component - using react bootstrap navbar
 function NavBar() {
     return (
+        <div>
         <Navbar bg="dark" variant="dark" expand="lg">
             <Navbar.Brand href="/"><h3>Local UP</h3></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -14,6 +15,19 @@ function NavBar() {
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
+        <Nav className="col-md-12 d-none d-md-block bg-dark sidebar">
+            <div className="sidebar-sticky"></div>
+        <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        </Nav>
+        </div>
     );
 }
 
