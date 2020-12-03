23a0c003f0397d45516ace90b2fa1d188653a466
samergain <samersadon@gmail.com>
2020-12-03 11:57:00 -0500

    server routes are working and auth signup and signin

--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -1,15 +1,46 @@
-const router = require("express").Router();
-const userController = require("../../controllers/user.controller");
+////////Samer version without authentication - tested and proven OK!//////////
+// const router = require("express").Router();
+// const userController = require("../../controllers/user.controller");
 
-// Matches with "/api/user"
-router.route("/")
-  .get(userController.findAll)
-  .post(userController.create);
+// // Matches with "/api/user"
+// router.route("/")
+//   .get(userController.findAll)
+//   .post(userController.create);
 
-// Matches with "/api/user/:id"
-router
-  .route("/:id")
-  .get(userController.findById)
-  .put(userController.update);
+// // Matches with "/api/user/:id"
+// router
+//   .route("/:id")
+//   .get(userController.findById)
+//   .put(userController.update);
 
-module.exports = router;
+// module.exports = router;
+
+///////tutorial version - testing mode////////
+const { authJwt } = require("../../middlewares");
+const controller = require("../../controllers/user.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.get("/api/test/all", controller.allAccess);
+
+  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+
+  // app.get(
+  //   "/api/test/mod",
+  //   [authJwt.verifyToken, authJwt.isModerator],
+  //   controller.moderatorBoard
+  // );
+
+  // app.get(
+  //   "/api/test/admin",
+  //   [authJwt.verifyToken, authJwt.isAdmin],
+  //   controller.adminBoard
+  // );
+};
