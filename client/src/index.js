a46513abb0dba9b8aab02a30fe48b8f67c1004ac
samergain <samersadon@gmail.com>
2020-12-03 23:07:13 -0500

    added services folder to client side

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,7 @@
 // import App from './App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
@@ -17,7 +13,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+
 // import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
---
+++ client/src/services/auth-service.js
@@ -0,0 +1,57 @@
+// source: https://bezkoder.com/react-jwt-auth/
+// The service uses Axios for HTTP requests and Local Storage for user information & JWT.
+// It provides following important methods:
+
+// login(): POST {username, password} & save JWT to Local Storage
+// logout(): remove JWT from Local Storage
+// register(): POST {username, email, password,...}
+// getCurrentUser(): get stored user information (including JWT)
+
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth/";
+
+class AuthService {
+  login(username, password) {
+    return axios
+      .post(API_URL + "signin", {
+        username,
+        password
+      })
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username, email, password, ...args) {
+    let name = args[0];
+    let company = args[1];
+    let contact = args[2];
+    let github = args[3];
+    let skills = args[4];
+    return axios.post(API_URL + "signup", {
+      username,
+      email,
+      password,
+      name,
+      company,
+      contact,
+      github,
+      skills
+    });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));;
+  }
+}
+
+export default new AuthService();
