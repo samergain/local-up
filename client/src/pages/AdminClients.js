93d1671fb93013b544c2d64d1805fca8f880a23a
Jeff T <jeff.thao92@gmail.com>
2020-12-04 11:30:05 -0600

    added some functions to grab the data for the client's information.

--- client/src/pages/AdminClients.js
+++ client/src/pages/AdminClients.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 // import {Link} from "react-router-dom";
-import { ClientCard, ClientCardBody } from "../components/AdminPortal/ClientsCard/ClientsCard";
+import {
+  ClientCard,
+  ClientCardBody,
+} from "../components/AdminPortal/ClientsCard/ClientsCard";
 import TicketList from "../components/AdminPortal/TicketList/TicketList";
 import ProjectButton from "../components/AdminPortal/ProjectButton/ProjectButton";
 // import {Input, TextArea, FormBtn} from "../components/AdminPortal/Form";
@@ -10,220 +13,212 @@ import tickets from "../data/tickets.json";
 import NavigationBar from "../components/AdminPortal/NavigationBar/NavigationBar";
 import NavSideBar from "../components/AdminPortal/NavSideBar/NavSideBar";
 
-
 function AdminClients() {
+  // const [clients, setClients] = useState([]);
 
-    // const [clients, setClients] = useState([]);
+  // useEffect(() => {
+  //     loadClients()
+  // }, [])
 
-    // useEffect(() => {
-    //     loadClients()
-    // }, [])
+  console.log("Clients Data: ", clients);
 
-    console.log("Clients Data: ", clients);
+  // function loadClients() {
+  //     console.log("API call to get all Clients");
+  //     // API.getAllClients()
+  //     // .then(res => {
+  //     //     setClients(res.data);
+  //     // })
+  //     // .catch(err => console.log(err));
+  // }
 
-    // function loadClients() {
-    //     console.log("API call to get all Clients");
-    //     // API.getAllClients()
-    //     // .then(res => {
-    //     //     setClients(res.data);
-    //     // })
-    //     // .catch(err => console.log(err));
-    // }
+  const [clientProfile, setClientProfile] = useState({
+    id: "",
+    name: "",
+    company: "",
+    email: "",
+    contact: "",
+    tickets: [],
+  });
 
-    const [clientProfile, setClientProfile] = useState({
-        id: "",
-        name: "",
-        company: "",
-        email: "",
-        contact: "",
-        tickets: []
+  function displayClientDtl(event, client) {
+    setTicketInfo({
+      id: "",
+      title: "",
+      type: "",
+      description: "",
     });
 
-    function displayClientDtl(event, client) {
+    setClientProfile({
+      id: client.id,
+      name: client.name,
+      company: client.company,
+      email: client.email,
+      contact: client.contact,
+      tickets: client.tickets,
+    });
+  }
 
+  const [ticketInfo, setTicketInfo] = useState({
+    id: "",
+    title: "",
+    type: "",
+    description: "",
+    projects: [],
+  });
+
+  function getTicketInfo(event, ticket) {
+    console.log("event clicked: ", event);
+    console.log("client clicked: ", ticket);
+    tickets.map((ticketdata) => {
+      if (ticketdata.id === ticket) {
         setTicketInfo({
-            id: "",
-            title: "",
-            type: "",
-            description: ""
+          id: ticketdata.id,
+          title: ticketdata.title,
+          type: ticketdata.type,
+          description: ticketdata.description,
         });
-
-        setClientProfile({
-            id: client.id,
-            name: client.name,
-            company: client.company,
-            email: client.email,
-            contact: client.contact,
-            tickets: client.tickets
-        });
-
-    }
-
-    const [ticketInfo, setTicketInfo] = useState({
-        id: "",
-        title: "",
-        type: "",
-        description: "",
-        projects: []
+      }
     });
+  }
 
-    function getTicketInfo(event, ticket) {
-        console.log("event clicked: ", event);
-        console.log("client clicked: ", ticket);
-        tickets.map((ticketdata) => {
-            if (ticketdata.id === ticket) {
-                setTicketInfo({
-                    id: ticketdata.id,
-                    title: ticketdata.title,
-                    type: ticketdata.type,
-                    description: ticketdata.description
-                });
-            }
-        })
-    }
+  // [project, setProject] = useState(false);
 
-    // [project, setProject] = useState(false);
+  // function createProjectForm(event, ticket){
+  //     console.log("create Project Form: ", ticket);
+  //     setProject(true);
+  // };
 
+  // const [projectForm, setProjectForm] = useState({
+  //     title: "",
+  //     description: "",
+  //     githubRepo: "",
+  //     createdBy: "",
+  //     status : "",
+  //     assignedUsers: [],
+  //     tasks: [],
+  //     ticket: []
+  // })
+  // function handleInputChange(event){
+  //     const { name, value } = event.target;
+  //     setProjectForm({...projectForm, [name]: value})
+  // };
 
-    // function createProjectForm(event, ticket){
-    //     console.log("create Project Form: ", ticket);
-    //     setProject(true);
-    // };
+  // function handleFormSubmit(event){
+  //     event.preventDefault();
+  //     if (projectForm.title && assignedUsers[0]) {
+  //         console.log("Saving Form: ", projectForm);
+  //     }
+  // };
 
-    // const [projectForm, setProjectForm] = useState({
-    //     title: "",
-    //     description: "",
-    //     githubRepo: "",
-    //     createdBy: "",
-    //     status : "",
-    //     assignedUsers: [],
-    //     tasks: [],
-    //     ticket: []
-    // })
-    // function handleInputChange(event){
-    //     const { name, value } = event.target;
-    //     setProjectForm({...projectForm, [name]: value})
-    // };
+  return (
+    <div>
+      <NavigationBar />
+      <Container fluid>
+        <Row className="show-grid">
+          <Col xs={2}>
+            <NavSideBar />
+          </Col>
+          <Col xs={3} lg={3}>
+            {!clients.length ? (
+              <h1 className="text-center">No Clients to Display</h1>
+            ) : (
+              <div className="card">
+                <ClientCard
+                  className="card-header"
+                  cardHeader="List of Clients"
+                  cardEntry="/clients"
+                >
+                  {clients.map((client) => {
+                    return (
+                      <ClientCardBody
+                        id={client.roles}
+                        key={client.roles}
+                        name={client.name}
+                        company={client.company}
+                        email={client.email}
+                        contact={client.contact}
+                        tickets={client.tickets}
+                        client={client}
+                        clickFunction={displayClientDtl}
+                      />
+                    );
+                  })}
+                </ClientCard>
+              </div>
+            )}
+          </Col>
 
-    // function handleFormSubmit(event){
-    //     event.preventDefault();
-    //     if (projectForm.title && assignedUsers[0]) {
-    //         console.log("Saving Form: ", projectForm);
-    //     }
-    // };
+          <Col xs={3} lg={3}>
+            {clientProfile.id !== "" ? (
+              <div className="card">
+                <div className="card-header mr-auto">
+                  <h2>{clientProfile.company}</h2>
+                </div>
+                <p>
+                  <strong>Contact:</strong> {clientProfile.name}
+                  <br />
+                  <strong>Email:</strong>
+                  {clientProfile.email}
+                  <br />
+                  <strong>Phone:</strong>
+                  {clientProfile.contact}
+                  <br />
+                </p>
+                <hr />
+                {clientProfile.tickets.length ? (
+                  <div className="card-body">
+                    <h6 className="text-center">Tickets:</h6>
 
-    return (
-        <div>
-            <NavigationBar />
-            <Container fluid>
-            <Row className="show-grid">
-                <Col xs={2}>
-                    <NavSideBar />
-                </Col>
-                    <Col xs={3} lg={3}>
-                        {!clients.length ? (
-                            <h1 className="text-center">No Clients to Display</h1>
-                        ) : (
-                                <div className="card">
-                                    <ClientCard
-                                        className="card-header"
-                                        cardHeader="List of Clients"
-                                        cardEntry="/clients"
-                                    >
-                                        {clients.map(client => {
-                                            return (<ClientCardBody
-                                                id={client.roles}
-                                                key={client.roles}
-                                                name={client.name}
-                                                company={client.company}
-                                                email={client.email}
-                                                contact={client.contact}
-                                                tickets={client.tickets}
-                                                client={client}
-                                                clickFunction={displayClientDtl}
-                                            />
-                                            );
-                                        })}
-                                    </ClientCard>
-                                </div>
-                            )}
-                    </Col>
-
-                    <Col xs={3} lg={3}>
-
-                        {(clientProfile.id !== "") ?
-                            (
-                                <div className="card">
-                                    <div className="card-header mr-auto">
-                                        <h2>{clientProfile.company}</h2>
-                                    </div>
-                                    <p>
-                                        <strong>Contact:</strong> {clientProfile.name}<br />
-                                        <strong>Email:</strong>{clientProfile.email}<br />
-                                        <strong>Phone:</strong>{clientProfile.contact}<br />
-                                    </p>
-                                    <hr />
-                                    {(clientProfile.tickets.length) ?
-                                        (
-                                            <div className="card-body">
-                                                <h6 className="text-center">Tickets:</h6>
-
-                                                {clientProfile.tickets.map(ticket =>
-                                                    <TicketList
-                                                        id={ticket}
-                                                        ticket={ticket}
-                                                        key={ticket}
-                                                        clickFunction={getTicketInfo}
-
-                                                    />
-                                                )
-                                                }
-
-                                            </div>
-                                        ) :
-                                        (
-                                            <div className="card-body">
-                                                <h6>No Tickets</h6>
-                                            </div>
-                                        )
-                                    }
-                                </div>
-                            ) :
-                            (<></>)
-                        }
-                    </Col>
-                    <Col xs={3} lg={3}>
-
-                        {(ticketInfo.id !== "") ?
-                            (
-                                <div className="card">
-                                    <div className="card-header mr-auto">
-                                        <h2>{ticketInfo.title}</h2>
-                                    </div>
-                                    <p>
-                                        <strong>Id:</strong>{ticketInfo.id}<br />
-                                        <strong>Type:</strong> {ticketInfo.type}<br />
-                                        <strong>Description:</strong>{ticketInfo.description}
-                                    </p>
-                                    <div className="card-body">
-                                        {/* <ProjectButton 
+                    {clientProfile.tickets.map((ticket) => (
+                      <TicketList
+                        id={ticket}
+                        ticket={ticket}
+                        key={ticket}
+                        clickFunction={getTicketInfo}
+                      />
+                    ))}
+                  </div>
+                ) : (
+                  <div className="card-body">
+                    <h6>No Tickets</h6>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <></>
+            )}
+          </Col>
+          <Col xs={3} lg={3}>
+            {ticketInfo.id !== "" ? (
+              <div className="card">
+                <div className="card-header mr-auto">
+                  <h2>{ticketInfo.title}</h2>
+                </div>
+                <p>
+                  <strong>Id:</strong>
+                  {ticketInfo.id}
+                  <br />
+                  <strong>Type:</strong> {ticketInfo.type}
+                  <br />
+                  <strong>Description:</strong>
+                  {ticketInfo.description}
+                </p>
+                <div className="card-body">
+                  {/* <ProjectButton 
                                         tktId={ticketInfo.id}
                                         tktType={ticketInfo.type}
                                         tktDesc={ticketInfo.description}
                                         clickFunction={createProjectForm}
                                         /> */}
-                                        <button className="btn btn-primary">
-                                            Create Project
-                                    </button>
-                                    </div>
-                                </div>
-                            ) :
-                            (<></>)
-                        }
-                    </Col>
-                    <Col xs={3} lg={3}>
-
-                        {/* {(project) ?
+                  <button className="btn btn-primary">Create Project</button>
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+          </Col>
+          <Col xs={3} lg={3}>
+            {/* {(project) ?
                             (
                                 <div className="card">
                                     <div className="card-header mr-auto">
@@ -263,11 +258,11 @@ function AdminClients() {
                             </div>):
                      (<></>)
                      } */}
-                    </Col>
-                </Row>
-            </Container>
-        </div >
-    )
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default AdminClients;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../components/client-portal/NavBar";
 import NavSideBar from "../components/client-portal/NavSideBar";
 import { Col, Container, Row } from "react-bootstrap";
+import clientUser from "../data/clients.json";
 
 function Profile() {
+
+  const [clientProfile, setClientProfile] = useState({
+    id: "1",
+    name: "",
+    company: "company",
+    email: "a@a.com",
+    contact: "Sam",
+    tickets: [],
+  });
+
+    const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+      loadClients()
+  }, [])
+
+   function loadClients() {
+      console.log("API call to get all Clients");
+      console.log(clientUser);
+      setClients(clientUser);
+      // API.getAllClients()
+      // .then(res => {
+      //     setClients(res.data);
+      // })
+      // .catch(err => console.log(err));
+  }
+
+
+
+  function displayClientDtl(event, client) {
+
+    setClientProfile({
+      id: client.id,
+      name: client.name,
+      company: client.company,
+      email: client.email,
+      contact: client.contact,
+      tickets: client.tickets,
+    });
+  }
+
+  // function getCurrentUser(id){
+  //   // let getUser = clients.filter(currUser => if (currUser.username ==id) return currUser);
+  //   //family.filter(person => person.age > 18);
+   
+  //   //set that to clientProfile
+  // }
+
+
   return (
     <div>
       <NavBar />
@@ -12,8 +62,89 @@ function Profile() {
           <Col xs={2}>
             <NavSideBar/>
           </Col>
-          <Col xs={10}>
-            <p>Hello User</p>
+          <Col xs={6}>
+            {clientUser[0].name}
+          {clientUser.id !== "" ? (
+              <div className="card mt-5">
+                <div className="card-header">
+                  <h2 className="text-center">{clientUser[0].name}</h2>
+                  <ul class="list-group">
+                  <li class="list-group-item text-center"> <strong className="text-left">Company: </strong>
+                  {clientProfile.company}</li>
+                  <li class="list-group-item text-center"><strong  className="text-left">Email: </strong> {clientUser.email}</li>
+                  <li class="list-group-item text-center"><strong className="text-left" >Contacts: </strong>
+                  {clientUser.contacts}</li>
+                </ul>
+                 {/*
+                </div>
+               
+               
+                <div className="justify-content-center">
+                  <p className=" pl-3 pt-2 ">
+                  <strong className="text-left">Company:</strong>
+                  {clientProfile.company}
+                  <br />
+                  <strong  className="text-left">Email:</strong> {clientProfile.email}
+                  <br />
+                  <strong className="text-left" >Contacts:</strong>
+                  {clientProfile.contacts}
+                </p></div>
+                
+                <div className="card-body">
+                   <ProjectButton 
+                                        tktId={ticketInfo.id}
+                                        tktType={ticketInfo.type}
+                                        tktDesc={ticketInfo.description}
+                                        clickFunction={createProjectForm}
+                                        /> */}
+                  <button className="btn btn-primary">Create Project</button>
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+          </Col>
+          <Col xs={4} lg={3}>
+            {/* {(project) ?
+                            (
+                                <div className="card">
+                                    <div className="card-header mr-auto">
+                                        <h2>{ticketInfo.title}</h2>
+                                    </div>
+                                    <div className="card-body">
+                                        <form>
+                                            <Input 
+                                            onChange={handleInputChange}
+                                            name="title"
+                                            placeholder="Title (required)"
+                                            value={projectForm.title}
+                                            />
+                                            <Input
+                                            onChange={handleInputChange}
+                                            name="description"
+                                            placeholder="Description (required)"
+                                            value={projectForm.description}
+                                            />
+                                            <Input
+                                            onChange={handleInputChange}
+                                            name="githubRepo"
+                                            placeholder="GitHubRepo (required)"
+                                            value={projectForm.githubRepo}
+                                            />
+                                            <Input
+                                            onChange={handleInputChange}
+                                            name="createdBy"
+                                            placeholder="Created By (required)"
+                                            value={projectForm.createdBy}
+                                            />
+                                            <Input
+                                            onChange={handleInputChange}
+                                            name="status"
+                                            placeholder="Status"
+                                </div>
+                            </div>):
+                     (<></>)
+                     } */}
 
           </Col>
         </Row>
