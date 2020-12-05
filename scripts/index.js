b633ac38aceeca581b1e3b042ab36684da0ccae9
Jeff T <jeff.thao92@gmail.com>
2020-12-04 21:45:08 -0600

    Added more data to the seeds

--- scripts/index.js
+++ scripts/index.js
@@ -10,33 +10,75 @@ mongoose.connect(
 
 const ClientSeed = [
   {
-    id: "1",
+    originalID: "1",
     company: "Test Company",
     name: "Mr. Bison",
+    password: "Test1",
     description:
       "We are a testing company",
     email: "mr.bison@streetfighter.com",
     contact: "651-123-4567",
+    roles: "ROLE_CLIENT",
     tickets: []
   },
   {
-    id: "2",
+    originalID: "2",
     company: "Tiger Test Knee",
     name: "Sagat",
+    password: "Test2",
     description:
       "Tiger Uppercut",
     email: "sagat@streetfighter.com",
     contact: "651-456-7890",
+    roles: "ROLE_CLIENT",
     tickets: []
   },
   {
-    id: "2",
+    originalID: "3",
     company: "Hadoken",
     name: "Ryu",
+    password: "Test3",
     description:
       "That spinning kick.",
     email: "ryu@streetfighter.com",
     contact: "651-789-0123",
+    roles: "ROLE_CLIENT",
+    tickets: []
+  },
+  {
+    originalID: "4",
+    company: "",
+    name: "Ken",
+    password: "Test3",
+    description:
+      "",
+    email: "ken@streetfighter.com",
+    contact: "123-456-7890",
+    roles: "ROLE_ADMIN",
+    tickets: []
+  },
+  {
+    originalID: "5",
+    company: "",
+    name: "Chung Li",
+    password: "Test3",
+    description:
+      "That spinning kick.",
+    email: "ryu@streetfighter.com",
+    contact: "651-789-0123",
+    roles: "ROLE_ADMIN",
+    tickets: []
+  },
+  {
+    originalID: "6",
+    company: "Hadoken",
+    name: "Ryu",
+    password: "Test3",
+    description:
+      "That spinning kick.",
+    email: "ryu@streetfighter.com",
+    contact: "651-789-0123",
+    roles: "ROLE_ADMIN",
     tickets: []
   }
 ];
