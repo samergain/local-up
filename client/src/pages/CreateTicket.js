3f2bb60389dc99b0088c6405b3b40608e27c5d65
samergain <samersadon@gmail.com>
2020-12-06 01:12:42 -0500

    linked tickets with their creators. once a ticket is created, its saved in the user also

--- client/src/pages/CreateTicket.js
+++ client/src/pages/CreateTicket.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { Col, Container, Row, Form, Button } from "react-bootstrap";
 import NavBar from "../components/client-portal/NavBar";
 import NavSideBar from "../components/client-portal/NavSideBar";
+import API from "../utils/API";
+import AuthService from "../services/auth-service";
 
 function CreateTicket() {
+  const currentUser = AuthService.getCurrentUser();
+  let ticketId = "";
+  const [form, setForm] = useState({});
+  const handleInputChange = function(e) {
+      const {name, value} = e.target;
+      console.log("here is the value of: ", value)
+      setForm({...form,[name]:value});
+  }
+  const handleSubmit = function(e) {
+      e.preventDefault();
+      
+      if (form.title && form.description){
+      API.saveTicket({title:form.title, description:form.description})
+         .then(response => {
+        ticketId = JSON.stringify(response._id);
+        API.addTicketToClient(currentUser.id,ticketId).then(response => console.log(response))
+      })
+      // .then(
+      //   API.addTicketToClient(currentUser.id,ticketId)
+      // )
+      .catch(err=> console.log(err))
+    }
+      
+  }
+
   return (
     <div>
       <NavBar />
@@ -21,15 +48,15 @@ function CreateTicket() {
               <div className="card-body">
               <Form.Group controlId="formTicketTitle">
                 <Form.Label>What's the issue? Give it a title.</Form.Label>
-                <Form.Control type="ticket" placeholder="Ticket Title" />
+                <Form.Control onChange={handleInputChange} name="title" type="text" placeholder="Ticket Title" />
               </Form.Group>
               <Form.Group controlId="exampleForm.ControlTextarea1">
                 <Form.Label>
                   What's the issue you are having? How would you like it fixed?
                 </Form.Label>
-                <Form.Control as="textarea" placeholder="Please describe in detail, what your issues are." rows={5} />
+                <Form.Control as="textarea" onChange={handleInputChange} name="description" placeholder="Please describe in detail, what your issues are." rows={5} />
               </Form.Group>
-              <Button variant="primary" type="submit">
+              <Button onClick={handleSubmit} variant="primary" type="submit">
                 Submit
               </Button>
               </div>
