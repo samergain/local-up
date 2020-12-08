3f6eac4bead5b6aba4e5856cfb9c2edf1d45bf7b
Meena Ambalam <meena.a.ambalam@gmail.com>
2020-12-08 01:51:45 -0600

    styling and color changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,28 +1,42 @@
 import React from "react";
 import NavBar from "../components/HomePortal/NavBar/NavBar";
 import { Container, Row, Col } from "react-bootstrap";
+import smallbusiness from "../images/localbusiness3.jpg";
 
 function Home() {
     return (
         <div>
             <NavBar />
             <Container fluid>
-                <Row className="justify-content-md-center">
-                    <Col xs={10}>
-                        <div className="card mainpage-card">
-                            <div className="mainpage">
-                            <h3>Home</h3>
-                                <label>LoremIpsum</label>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+
+                <div className="blue-container-homepage">
+                    <Row className="justify-content-md-center">
+
+                        
+
+                        <Col xs={7} >
+                            <div className="card homepage-card">
+                                <div className="homepage">
+                                    <label><h1>Local UP</h1></label>
+                                    <p>
+                                        Lorem ipsum dolor sit amet,
+                                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                </Row>
+                        </Col>
+
+                        <Col xs={3} >
+                            <div className="img-card-homepage ">
+                                <div className="text-center">
+                                    <img src={smallbusiness} alt="Website Builder" className="mainpage-image" />
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+
+
             </Container>
         </div>
     );
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,6 +1,9 @@
 import React from "react";
 import NavBar from "../components/HomePortal/NavBar/NavBar";
 import { Container, Row, Col } from "react-bootstrap";
+import websiteimage from "../images/website4.jpg";
+import seo from "../images/SEO2.jpg";
+import onlinemarketing from "../images/online_marketing.jpg";
 
 
 function Services() {
@@ -8,23 +11,84 @@ function Services() {
         <div>
             <NavBar />
             <Container fluid>
-                <Row className="justify-content-md-center">
-                    <Col xs={10} >
-                        
-                        <div className="card mainpage-card">
-                            <div className="mainpage">
-                                <h3>Services</h3>
-                                <label>LoremIpsum</label>
-                                <p>
-                                    Lorem ipsum dolor sit amet,
-                                    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
+
+                <div className="blue-container">
+                    <Row className="justify-content-md-center">
+                       
+                        <Col xs={8} >
+                            <div className="card mainpage-card">
+                                <div className="mainpage">
+                                    <label>Website Building</label>
+                                    <p>
+                                        Lorem ipsum dolor sit amet,
+                                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                </Row>
+                        </Col>
+                        <Col xs={2} >
+                            <div className="img-card text-center">
+                                <div className="mainpage">
+                                    <img src={websiteimage} alt="Website Builder" className="mainpage-image" />
+                                </div>
+                            </div>
+                        </Col>
+
+                    </Row>
+                </div>
+
+                <div className="blue-container">
+                    <Row className="justify-content-md-center">
+
+                        <Col xs={3} >
+                            <div className="img-card text-center">
+                                <div className="mainpage">
+                                    <img src={seo} alt="Website Builder" className="mainpage-image" />
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={8} >
+                            <div className="card mainpage-card">
+                                <div className="mainpage">
+                                    <label>SEO (Search Engine Optimization)</label>
+                                    <p>
+                                        Lorem ipsum dolor sit amet,
+                                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                                </div>
+                            </div>
+                        </Col>
+
+                    </Row>
+                </div>
+
+                <div className="blue-container">
+                    <Row className="justify-content-md-center">
+                       
+                        <Col xs={8} >
+                            <div className="card mainpage-card">
+                                <div className="mainpage">
+                                    <label>Online Marketing Building</label>
+                                    <p>
+                                        Lorem ipsum dolor sit amet,
+                                        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col xs={2} >
+                            <div className="img-card text-center">
+                                <div className="mainpage">
+                                    <img src={onlinemarketing} alt="Website Builder" className="mainpage-image" />
+                                </div>
+                            </div>
+                        </Col>
+
+                    </Row>
+                </div>
+
             </Container>
-        </div>
+        </div >
     );
 }
 
