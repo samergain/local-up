de0edabf6b89f75b17827cce25a4ddd88de30001
samergain <samersadon@gmail.com>
2021-01-01 18:42:51 -0500

    resolving files version conflict

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -3,15 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-<<<<<<< HEAD
-    <div class="text-center d-flex">
-      <div class="p-3">
-        <p>COPYRIGHT 2020 LOCALUP</p>
-=======
     <div className="text-center d-flex bg-dark">
       <div className="p-3">
         <p className="text-color">COPYRIGHT 2020 LOCALUP</p>
->>>>>>> 153b7d475662ce2da0d28bb9a2b952c03cc043c4
       </div>
     </div>
   </footer>
