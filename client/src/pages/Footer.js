c5b741120c53771de76195513f7026e9cdd7e975
samergain <samersadon@gmail.com>
2021-01-01 18:20:41 -0500

    updating files

---
+++ client/src/pages/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+    <div class="text-center d-flex">
+      <div class="p-3">
+        <p>COPYRIGHT 2020 LOCALUP</p>
+      </div>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
