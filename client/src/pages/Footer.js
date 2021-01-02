e861c87c7c1b06f49eb0f1077fe6c55251d2babe
samergain <samersadon@gmail.com>
2021-01-01 19:03:07 -0500

    editing index.css

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -3,12 +3,12 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-    <div className="text-center d-flex bg-dark">
-      <div className="p-3">
-        <p className="text-color">COPYRIGHT 2020 LOCALUP</p>
+      <div className="text-center d-flex bg-dark">
+        <div className="p-3">
+          <p className="text-color">COPYRIGHT 2020 LOCALUP</p>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
   );
 }
 
